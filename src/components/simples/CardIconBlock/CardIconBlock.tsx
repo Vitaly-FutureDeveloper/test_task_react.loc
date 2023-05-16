6b46b941d36455c0bd00eb3ae34fa04b6ac6f90e
Vitaliy-Future-Developer <zarvoron@yandex.ru>
2023-05-16 12:43:34 +0300

    background-color in icon card

--- src/components/simples/CardIconBlock/CardIconBlock.tsx
+++ src/components/simples/CardIconBlock/CardIconBlock.tsx
@@ -1,17 +1,31 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import styles from "./CardIconBlock.module.scss"
 import {useSelector} from "react-redux"
 import {NavLink} from "react-router-dom"
 import {getProductCard} from "../../../redux/card-reducer/card-selectors"
 
 export const CardIconBlock: React.FC = () => {
+	const BACKGROUND_ICON = {
+		zero: "#b6b6b6",
+		many: "#008600",
+	};
 
 	const productCard = useSelector(getProductCard)
+
+	const [zeroLengthStyle, setZeroLengthStyle] = useState({backgroundColor: BACKGROUND_ICON.zero})
+
+	useEffect(() => {
+		if(productCard.length === 0) {
+			setZeroLengthStyle({backgroundColor: BACKGROUND_ICON.zero})
+		} else {
+			setZeroLengthStyle({backgroundColor: BACKGROUND_ICON.many})
+		}
+	}, [productCard])
 
 	return (
 		<NavLink to={"/card"}>
 			<div className={styles.cardIconBlock}>
-				<span className={styles.cardIconBlock__count}>{productCard.length}</span>
+				<span className={styles.cardIconBlock__count} style={zeroLengthStyle}>{productCard.length}</span>
 			</div>
 		</NavLink>
 	)
